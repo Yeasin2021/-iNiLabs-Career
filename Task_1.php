832b828ca75bb7068fba48008564107ee4f7c101
unknown <yeasin18801@gmail.com>
2023-08-18 16:30:55 +0600

    Task-1

---
+++ Task_1.php
@@ -0,0 +1,39 @@
+<?php
+class Shape {
+    public function calculateArea() {
+        // This method will be overridden in subclasses
+    }
+}
+
+class Circle extends Shape {
+    public $radius;
+
+    public function __construct($radius) {
+        $this->radius = $radius;
+    }
+
+    public function calculateArea() {
+        return pi() * $this->radius * $this->radius;
+    }
+}
+
+class Rectangle extends Shape {
+    public $width;
+    public $height;
+
+    public function __construct($width, $height) {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function calculateArea() {
+        return $this->width * $this->height;
+    }
+}
+
+// Example usage
+$circle = new Circle(5);
+$rectangle = new Rectangle(4, 6);
+
+echo "Area of the circle with radius {$circle->radius}: {$circle->calculateArea()}<br>";
+echo "Area of the rectangle with width {$rectangle->width} and height {$rectangle->height}: {$rectangle->calculateArea()}";
